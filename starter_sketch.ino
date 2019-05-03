a507000828f6926d97e6efe212d5b776a2174b9c
lukeaboudi <50246011+lukeaboudi@users.noreply.github.com>
2019-05-03 14:26:28 -0400

    Update starter_sketch.ino

--- starter_sketch.ino
+++ starter_sketch.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+  Blink
+ */
 
+void setup () {
+    pinMode(13,OUTPUT);
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
 
 }
